fe670ce6df20f4aaf098c4a713feb927761be219
ashukla333 <amanshukla3747@gmail.com>
2024-08-30 21:13:40 +0530

    crud auth done

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ import express from "express";
 import userRouter from "./routes/user.js";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 export const app = express();
 configDotenv({
   path: "./data/config.env",
 });
 
+app.use(
+  cors({
+    origin: ["https://kigsvillah.vercel.app", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use("/v1", userRouter);
